1e7c7c3121db999665224e8374c3487f204d3080
nazrinmajidova <150728280+nazrinmajidova@users.noreply.github.com>
2024-07-30 07:05:44 +0400

    Extension Function

---
+++ Homework/CustomMethod/Array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System;
+
+public static class Array
+{
+    public static void CustomResize<T>(ref T[] array, params T[] newElement)
+    {
+        int arrLength = array.Length;
+        int newLength = arrLength + newElement.Length;
+        T[] newArray = new T[newLength];
+
+        for(int i = 0 ; i < arrLength; i++ )
+        {
+            newArray[i] = array[i];
+        }
+        for (int i = 0; i < newElement.Length; i++)
+        {
+            newArray[i + arrLength] = newElement[i];
+        }
+        array = newArray;
+
+    }
+
+}
---
+++ Homework/CustomMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CustomMethod
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, };
+            Console.WriteLine(string.Join(" ", array));
+
+            Array.CustomResize(ref array, 6, 7, 8, 9, 10);
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+       
+}
